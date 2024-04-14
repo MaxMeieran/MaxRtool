bff53a36bc5e665b928e533fd766d4dd75ce40e7
An Haoyuan <103879569+MaxMeieran@users.noreply.github.com>
2024-04-14 17:14:19 +0800

    Update add_score.R

--- R/add_score.R
+++ R/add_score.R
@@ -4,28 +4,27 @@ add_score <- function(seurat_obj, gene_list) {
   if (is.null(seurat_obj) || !is(seurat_obj, "Seurat")) {
     stop("Invalid Seurat object provided.")
   }
-
+  
   if (is.null(gene_list) || !is.list(gene_list)) {
     stop("Invalid gene list provided.")
   }
-
+  
   # 对于基因列表中的每个细胞类型
   for (cell_type in names(gene_list)) {
     genes <- gene_list[[cell_type]]
-
+    
     # 检查基因是否在Seurat对象中
     if (!all(genes %in% rownames(seurat_obj))) {
       warning(paste("Some genes for", cell_type, "are not found in the Seurat object. Skipping this cell type."))
-
+      
     }
-
+    
     # 计算并添加模块分数
     seurat_obj <- AddModuleScore(
       object = seurat_obj,
-      features = list(genes)
-
+      features = list(genes),name=cell_type
+      
     )
   }
-
   return(seurat_obj)
 }
